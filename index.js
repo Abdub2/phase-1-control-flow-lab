6cbe993d44e1f76d122c4b8645bbef3bd1dc1f56
Abdub2 <abdubdoyo2005@gmail.com>
2022-11-23 16:11:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(d){
+  let result;
+  if(d<=400){
+    result="This one is on me!"
+  }else if(d>2500){
+    result="No can do."
+  }else if(d>2000){
+    result="I will gladly take your thirty bucks."
+   }
+  return result;
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
+scuberGreetingForFeet(2501);
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+  const cityMessage=city==="NYC" ? "Ok, sounds good." : "No go.";
+  return cityMessage;
 }
+console.log(ternaryCheckCity("NYC"))
+console.log(ternaryCheckCity("Pittsburgh"))
+
+function switchOnCharmFromTip(tip){
+  let response;
+switch(tip){
+  case 'generous':
+    return'Thank you so much.';
+  case 'not as generous':
+    return('Thank you.');
+  default:
+    return'Bye.'
+}
+}
+console.log(switchOnCharmFromTip("generous"));
+console.log(switchOnCharmFromTip("not as generous"));
+console.log(switchOnCharmFromTip("thanks for everything"));
